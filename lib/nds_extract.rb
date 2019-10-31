525347e3461b050e8c56cb7241d5bd523373ba07
Anthony Pardo <pardo.anthony@gmail.com>
2019-10-31 19:38:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def directors_totals(nds)
   result = {}
   row_index = 0 
   while row_index < nds.length do 
-    gross_for_director(nds[row_index][:name])
+    gross_for_director(nds[row_index][:movies])
     row_index += 1 
   end 
   result
@@ -18,5 +18,5 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  if director_data == 
+  puts director_data
 end
